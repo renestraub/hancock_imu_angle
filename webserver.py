9b9fecc6ab8e63462a55629ecebadd96dc3454a8
Rene Straub <rene.straub@netmodule.com>
2021-02-20 09:34:09 +0100

    initial commit

---
+++ webserver.py
@@ -0,0 +1,39 @@
+
+import subprocess
+import os
+import sys
+
+
+
+# https://github.com/Nakiami/MultithreadedSimpleHTTPServer/blob/master/MultithreadedSimpleHTTPServer.py
+
+try:
+    # Python 2.x
+    from SocketServer import ThreadingMixIn
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    from BaseHTTPServer import HTTPServer
+except ImportError:
+    # Python 3.x
+    from socketserver import ThreadingMixIn
+    from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
+
+
+def main():
+    # cwd = os.getcwd()
+    
+    print("Starting HTTP Server on port 8888")
+    server = ThreadingSimpleServer(('', 8888), SimpleHTTPRequestHandler)
+    
+    try:
+        while 1:
+            sys.stdout.flush()
+            server.handle_request()
+    except KeyboardInterrupt:
+        print("Finished")
+
+    
+main()
